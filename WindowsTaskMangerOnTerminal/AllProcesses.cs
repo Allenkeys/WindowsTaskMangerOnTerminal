17bcc97d6da10ed87f270b84c723952642214b31
Joshua Eze <ezejoshuac@gmail.com>
2022-12-14 05:59:49 +0100

    Add project files.

---
+++ WindowsTaskMangerOnTerminal/AllProcesses.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace WindowsTaskMangerOnTerminal
+{
+    internal class AllProcesses
+    {
+        public static void EnumerateProcesses()
+        {
+            var allRunningProcesses = from procs in Process.GetProcesses()
+                                      orderby procs.Id
+                                      select procs;
+            foreach (var item in allRunningProcesses)
+            {
+                Console.WriteLine($"Process Id:{item.Id}\nName: {item.ProcessName}\n");
+                Console.WriteLine("**************************************\n");
+            }
+        }
+    }
+}
